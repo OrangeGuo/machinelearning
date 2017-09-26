803de68ddb4e3e84e22153aee71f1c8b632810b6
OrangeGuo <1772086465@qq.com>
2017-09-26 20:07:03 +0800

    more instance

--- qianceng.py
+++ qianceng.py
@@ -1,15 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import sklearn
-import sklearn.datasets
 import sklearn.linear_model
-import matplotlib
-
-#np.random.seed(0)
+import sklearn.datasets
+np.random.seed(0)
 X, y = sklearn.datasets.make_moons(200, noise=0.20)
 
 plt.scatter(X[:, 0], X[:, 1], s=40, c=y, cmap=plt.cm.Spectral)
-# plt.show()
+ # plt.show()
 
 clf = sklearn.linear_model.LogisticRegressionCV()
 clf.fit(X, y)
