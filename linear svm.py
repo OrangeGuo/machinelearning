9ba3c2c68e1393ab327c13a9d38e2926050da342
OrangeGuo <1772086465@qq.com>
2017-10-08 14:42:14 +0800

    push some changes

--- linear svm.py
+++ linear svm.py
@@ -1,11 +1,19 @@
 from sklearn import svm
+import matplotlib.pyplot as plt
+import numpy as np
 
-x = [[2, 0, 1], [1, 1, 2], [2, 3, 3]]
-y = [0, 0, 1]
-clf = svm.SVC(kernel='linear')
-clf.fit(x,y)
-print clf
-print clf.support_vectors_
-print clf.support_
-print clf.n_support_
-print clf.predict([[2,0,3]])
+x = np.r_[[[0, 1], [1, 1], [1, 0]]]
+y = [0, 1, 2]
+# clf = svm.SVC(kernel='linear')
+# clf.fit(x, y)
+#
+# print clf.support_vectors_
+# print clf.support_
+# print clf.n_support_
+# print clf.predict([[0, 2]])
+# plt.plot([0,1],[1,0])
+# plt.show()
+print x[:,0]
+print x[:,1]
+plt.scatter(x[:,0],x[:,1],c=y)
+plt.show()
--- linears svm plt.py
+++ linears svm plt.py
@@ -3,19 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 np.random.seed(0)
-x = np.r_[np.random.randn(20,2)-[2,2],np.random.randn(20,2)+[2,2]]
+x = np.r_[np.random.randn(20,2)-[2,2],np.random.randn(20,2)+[2,2]] # funciton like reshape
 # x =[ np.random.randn(20,2)-[2,2]]*1+[np.random.randn(20,2)+[2,2]]*1
-
 y = [0]*20 + [1]*20
-
 clf = svm.SVC(kernel='linear')
 clf.fit(x,y)
 w = clf.coef_[0] #faxiangliang
 a = -w[0]/w[1]
 xx = np.linspace(-5,5)
 yy = a*xx-(clf.intercept_[0])/w[1]
-print xx
-print yy
 b = clf.support_vectors_[0]
 yy_down = a*xx + (b[1]-a*b[0])
 b = clf.support_vectors_[-1]
@@ -25,11 +21,10 @@ print "W:",w
 print "a:",a
 print "support_vectors_:",clf.support_vectors_
 print "clf.coef_:",clf.coef_
-
-plt.figure(figsize=(8,4))
+plt.figure(figsize=(8,8))
 plt.plot(xx,yy)
-# plt.plot(xx,yy_down)
-# plt.plot(xx,yy_up)
+plt.plot(xx,yy_down)
+plt.plot(xx,yy_up)
 plt.scatter(clf.support_vectors_[:,0],clf.support_vectors_[:,1],s=80)
 plt.scatter(x[:,0],x[:,1],c=y,cmap=plt.cm.Paired)
 plt.axis('tight')
