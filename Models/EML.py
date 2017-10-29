8e2c3a89b318c0fe200ff6755f4145a04d066386
OrangeGuo <1772086465@qq.com>
2017-10-29 11:32:55 +0800

    complete Extreme Learning Machine

--- Models/EML.py
+++ Models/EML.py
@@ -24,7 +24,19 @@ y =np.array([y]).T
 input_weights = 2*np.random.random((input_layers_num,hidden_layers_num)) - 1
 hidden = X.dot(input_weights)
 output_weights = (mat(hidden)).I.dot(y)
-sample =np.array([[100]])
-print sample.dot(input_weights).dot(output_weights)
-
+sample = np.zeros((loop,1))
+for i in range(len(X)):
+    if i==0:
+        sample[0][0] = X[loop-1][0]+loop
+    else:
+        sample[i][0] = sample[i-1][0]+loop
+    loop +=1
+result = sample.dot(input_weights).dot(output_weights)
+with open('ELM.txt', 'w') as file:
+    s = '\t\r'.join(str(i[0]) for i in (np.array(y)))
+    file.write(s)
+    file.write('\r')
+    s = '\t\r'.join(str(i[0]) for i in (np.array(result)))
+    file.write(s)
+    file.close()
 
